95ef04aa448ae79374a20ba718ec066c9960f0d1
Merry Wonson <meredithwonson@gmail.com>
2025-03-14 12:31:46 -0400

    backend running (getPosts works)

--- backend/server/routes/posts/post.getAllPosts.js
+++ backend/server/routes/posts/post.getAllPosts.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const newPostModel = require('../../models/postModel')
 
-router.get('/posts/getAllPosts', async (req, res) => {
+router.get('/posts', async (req, res) => {
     const allPosts = await newPostModel.find()
     return res.status(200).json(allPosts)
     
