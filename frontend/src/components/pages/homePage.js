455c66074f88d9a1b9727d2149649c4b9a423ded
mx-vin <spraggins0602@gmail.com>
2025-04-11 12:37:12 -0400

    sidebar, navbar, profile page

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { FaHome, FaUser, FaBell, FaEnvelope, FaHashtag, FaBookmark, FaUsers, FaCrown, FaBolt, FaUserCircle, FaEllipsisH, FaFeatherAlt } from 'react-icons/fa';
 
 const HomePage = () => {
@@ -22,16 +23,16 @@ const HomePage = () => {
         <div className="space-y-6">
           <FaFeatherAlt className="text-3xl mb-4" />
           <div className="space-y-4">
-            <NavItem icon={<FaHome />} label="Home" />
-            <NavItem icon={<FaHashtag />} label="Explore" />
-            <NavItem icon={<FaBell />} label="Notifications" />
-            <NavItem icon={<FaEnvelope />} label="Messages" />
-            <NavItem icon={<FaBookmark />} label="Bookmarks" />
-            <NavItem icon={<FaUsers />} label="Communities" />
-            <NavItem icon={<FaCrown />} label="Premium" />
-            <NavItem icon={<FaBolt />} label="Verified Orgs" />
-            <NavItem icon={<FaUser />} label="Profile" />
-            <NavItem icon={<FaEllipsisH />} label="More" />
+            <NavItem to="/" icon={<FaHome />} label="Home" />
+            <NavItem to="/" icon={<FaHashtag />} label="Explore" />
+            <NavItem to="/" icon={<FaBell />} label="Notifications" />
+            <NavItem to="/" icon={<FaEnvelope />} label="Messages" />
+            <NavItem to="/" icon={<FaBookmark />} label="Bookmarks" />
+            <NavItem to="/" icon={<FaUsers />} label="Communities" />
+            <NavItem to="/" icon={<FaCrown />} label="Premium" />
+            <NavItem to="/" icon={<FaBolt />} label="Verified Orgs" />
+            <NavItem to="/profile" icon={<FaUser />} label="Profile" />
+            <NavItem to="/" icon={<FaEllipsisH />} label="More" />
           </div>
           <button className="mt-6 bg-white text-purple-600 font-bold py-2 rounded-full w-full hover:bg-gray-100 transition">Post</button>
         </div>
