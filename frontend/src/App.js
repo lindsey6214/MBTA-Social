f9118c2d3eb52b4409b009b4e838367bafe737cf
mx-vin <spraggins0602@gmail.com>
2025-04-19 17:56:07 -0400

    app.js minor fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import LandingPage from "./components/pages/landingPage";
 import HomePage from "./components/pages/homePage";
 import Login from "./components/pages/loginPage";
 import Signup from "./components/pages/registerPage";
-import PrivateUserProfile from "./components/pages/privateUserProfilePage";
+import UserProfile from "./components/pages/userProfilePage";
 import ExplorePage from "./components/pages/explorePage";
 import NotificationsPage from "./components/pages/notificationsPage";
 import MessagesPage from "./components/pages/messagesPage";
@@ -36,7 +36,7 @@ const App = () => {
           <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
+          <Route path="/profile" element={<UserProfile />} />
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/notifications" element={<NotificationsPage />} />
           <Route path="/messages" element={<MessagesPage />} />
